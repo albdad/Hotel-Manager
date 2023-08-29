304a4d31d1f8559dd92a523a592d3e2352fea92a
albdad <108136775+albdad@users.noreply.github.com>
2023-08-29 19:02:57 +0200

    Initial commit

---
+++ src/test/java/hotel_manager/HotelManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package hotel_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
